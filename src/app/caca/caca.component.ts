a80e747af239e46f076b83e0a5370096856fc701
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2016-10-18 21:07:01 +0200

    agregem el service al component i fem el http get al carregar el component

--- src/app/caca/caca.component.ts
+++ src/app/caca/caca.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'app-caca',
   templateUrl: './caca.component.html',
-  styleUrls: ['./caca.component.css']
+  styleUrls: ['./caca.component.css'],
+  providers: [TestService]
 })
 export class CacaComponent implements OnInit {
   @Input() hero: String;
+  lol: any;
+  errorMessage: string;
   constructor() { }
 
   ngOnInit() {
+    this.service.getTest().subscribe(
+        (lol) => {
+          console.log(lol)
+          this.lol = lol
+        },
+        error =>  this.errorMessage = <any>error
+    );
   }
 
 }
