87a9c106dbee10e04a81ea5794907c82061d82b0
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2016-10-18 20:57:07 +0200

    importem el http i el rxjs

--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable()
 export class TestService {
