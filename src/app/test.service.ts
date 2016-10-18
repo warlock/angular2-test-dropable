d29acfbbe72231c6b85f4e4c38ebcd0fb0fc2e70
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2016-10-18 20:58:20 +0200

    metode per error als request, i l altre per transformar el json en un objecte en javascript .json()

--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -8,4 +8,16 @@ export class TestService {
 
   constructor() { }
 
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: any) {
+    console.log(error)
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
 }
